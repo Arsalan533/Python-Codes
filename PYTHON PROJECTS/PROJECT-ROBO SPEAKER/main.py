74daaec45e157321f7339e02322af99796d6a8b2
Arsalan533 <arsalanjamal533@gmail.com>
2025-02-11 14:00:48 +0500

    index.py

---
+++ PYTHON PROJECTS/PROJECT-ROBO SPEAKER/main.py
@@ -0,0 +1,19 @@
+import os
+
+if __name__ == '__main__':
+    print("welcome to RoboSpeaker 2.0 Created by Arsalan")
+    while True:
+        x = input("Enter what you want to me speak: ")
+        if x =="q":
+            os.system("espeak -s 130 'bye bye friend'")
+            break
+        command = f'espeak -s 130 "{x}"'
+        os.system(command)
+
+
+
+
+
+
+
+
